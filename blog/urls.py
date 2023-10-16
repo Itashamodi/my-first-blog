4595468b05a48675b7d8f0dbf8b1c5960549c847
Itasha <modiitasha@gmail.com>
2023-10-16 07:36:33 +0530

    Added view to create/edit blog post

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 if settings.DEBUG:
     urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
